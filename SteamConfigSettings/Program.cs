ee5cee0b3fec4677835d9e305779a0766e297faa
Daniel Woods <dibwoods@gmail.com>
2018-01-17 15:00:00 +0000

    Add project files.

---
+++ SteamConfigSettings/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SteamConfigHelper {
+    class Program {
+        static void Main(string[] args) {
+            /*
+            SteamLocalConfig config = new SteamLocalConfig();
+            config.SetAppSetting("507490", "LaunchOptions", "/nolaunch");
+            if (config.GetAppSetting("507490", "LaunchOptions", out string value)) {
+                Console.WriteLine(value);
+            }
+            */
+        }
+    }
+}
